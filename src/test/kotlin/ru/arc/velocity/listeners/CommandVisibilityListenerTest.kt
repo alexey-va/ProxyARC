e55d8f406cc05310d57916a927407e72868f4617
alexey-va <freedeeml@gmail.com>
2026-09-02 10:13:09 +0300

    Hide owner-selected technical commands

--- src/test/kotlin/ru/arc/velocity/listeners/CommandVisibilityListenerTest.kt
+++ src/test/kotlin/ru/arc/velocity/listeners/CommandVisibilityListenerTest.kt
@@ -6,8 +6,12 @@ import com.velocitypowered.api.event.command.PlayerAvailableCommandsEvent
 import com.velocitypowered.api.proxy.Player
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import ru.arc.config.Config
+import ru.arc.config.ConfigManager
+import java.nio.file.Files
 
 class CommandVisibilityListenerTest :
     FreeSpec({
@@ -28,6 +32,59 @@ class CommandVisibilityListenerTest :
 
             root.children.map { it.name } shouldContainExactlyInAnyOrder
                 listOf("world", "litebans:litebans", "home")
+        }
+
+        "bundled policy hides owner-selected technical roots and keeps gameplay roots" {
+            val directory = Files.createTempDirectory("proxyarc-command-visibility-")
+            Config.copyDefaultConfig(
+                ConfigManager.bundledModuleResource("command-visibility.yml"),
+                directory,
+                replace = false,
+            )
+            val settings = CommandVisibilityConfig(ConfigManager.ofModule(directory, "command-visibility.yml"))
+            val hidden = settings.hiddenRoots
+
+            hidden.containsAll(
+                setOf(
+                    "br",
+                    "brush",
+                    "desel",
+                    "deselect",
+                    "sel",
+                    "toggleplace",
+                    "none",
+                    "pos",
+                    "tool",
+                    "auth",
+                    "botfilter",
+                    "limboauth",
+                    "limbofilter",
+                    "luckpermsvelocity",
+                    "lpv",
+                    "planbungee",
+                    "planproxy",
+                    "planvelocity",
+                    "creative",
+                    "god",
+                    "ungod",
+                    "heal",
+                    "tangledmaze",
+                    "maze",
+                    "gravesx",
+                ),
+            ) shouldBe true
+            hidden.intersect(
+                setOf(
+                    "arc",
+                    "builder",
+                    "cmi",
+                    "hack",
+                    "infinityexpansion",
+                    "geneticchickengineering",
+                    "foxymachines",
+                    "emctech",
+                ),
+            ) shouldBe emptySet()
         }
     })
 
