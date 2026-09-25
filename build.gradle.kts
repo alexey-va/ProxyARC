5d3dee02f373176f75c8a7eea1279c09c674d9bf
alexey-va <freedeeml@gmail.com>
2026-09-25 21:05:08 +0300

    build: update arc-core to 2.7.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,13 @@ repositories {
 }
 
 dependencies {
-    implementation("ru.ruscrafting.arc:arc-core:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-metrics:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-velocity:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-ai:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.11")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-metrics:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-velocity:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-ai:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.13")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.10.2")
 
@@ -69,7 +69,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.11")
+    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.13")
     configurations["integrationTestImplementation"].extendsFrom(configurations["testImplementation"])
     configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
 }
